33c68381ca6134d4ac6c2bc61b7b1fe3a2c0e52c
Nicolas North <nikksno@users.noreply.github.com>
2017-07-20 12:09:33 +0200

    deleted old stuff

--- MacHete/02 | Software and Scripts/2 | FirstBoot/1 | Sources/E MDM Enroller/2 | Installer script/MDM_Enroller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo /usr/bin/profiles -I -F /Library/Scripts/A-Trust_Profile_for_evilcorp.mobileconfig
-
-sleep 4
-
-sudo /usr/bin/profiles -I -F /Library/Scripts/B-Mac_Enrollment_Profile.mobileconfig
